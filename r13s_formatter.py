1e3e6dd45fb3c2c7ea5c2e52f2a411ba006accb0
Thanh Truong <hopchuathurac@gmail.com>
2018-10-04 01:14:30 +0200

    export recommendations with movies / series

---
+++ r13s_formatter.py
@@ -0,0 +1,85 @@
+import json
+import copy
+import pprint
+
+DATA_ITEM_ID = 'id'
+DATA_ITEM_TYPE = 'type'
+DATA_ITEM_DATA = 'data'
+MOVIE = 'movie'
+SERIES = 'series'
+R13S_CONFIG_JSON = 'r13s_config.json'
+RECOMMENDATIONS = 'recommendations'
+
+def __validate(json, json_sample):
+    for key, value in json_sample.items():
+        acutal_value = json[key]
+        assert((type(acutal_value) is type(value)) 
+                or (type(acutal_value).__name__ == 'unicode' 
+                    and  type(value).__name__ == 'str')
+                or (type(acutal_value).__name__ == 'str' 
+                    and  type(value).__name__ == 'unicode')) 
+
+def __load_sample_r13s_formats(r13s_filename=R13S_CONFIG_JSON):
+    with open(r13s_filename, "r") as f:
+        return json.load(f)
+
+def __format_item(item, json_sample_dict):
+    assert(DATA_ITEM_TYPE in item and item[DATA_ITEM_TYPE])
+    item_type = item[DATA_ITEM_TYPE]
+    formatted_item = copy.deepcopy(json_sample_dict[item_type])
+    formatted_item.pop(RECOMMENDATIONS, None)
+    if (MOVIE == item_type):
+        formatted_item["video_id"] = item[DATA_ITEM_ID]
+    elif (SERIES == item_type):
+        formatted_item["series_id"] = item[DATA_ITEM_ID]
+    else:
+        assert(False)
+
+    return formatted_item
+
+
+def validate(r13s, config_file = R13S_CONFIG_JSON):
+    "Validate a JSON (recommendations, r13s) accordingly to a configuration"
+    formats = __load_sample_r13s_formats(config_file)
+    # validate all top-level fields
+    __validate(r13s, formats['r13s_format'])
+
+    # validate all items in data field
+    for data_item in r13s[DATA_ITEM_DATA]:
+        assert(DATA_ITEM_TYPE in data_item)
+        data_item_type = data_item[DATA_ITEM_TYPE]
+        __validate(data_item, formats[data_item_type])
+
+def format_recommendation(item, associated_items=[], json_sample_dict={}, 
+                        config_file = R13S_CONFIG_JSON):
+    "Format a recommendation consisting of an item and its associated items"
+    # load conifgurations if not provided 
+    if len(json_sample_dict) == 0:
+        json_sample_dict = __load_sample_r13s_formats(config_file)
+
+    formatted_item = __format_item(item, json_sample_dict)
+    # associated_items
+    if (len(associated_items) != 0):
+        formatted_item[RECOMMENDATIONS] = [__format_item(i, json_sample_dict) 
+                                        for i in associated_items]
+    return formatted_item
+
+def format_r13s(config_file = R13S_CONFIG_JSON):
+    "Format recommendations"
+    json_sample_dict = __load_sample_r13s_formats(config_file)
+    return json_sample_dict['r13s_format']
+
+def main():
+    r13s = format_r13s()
+    data = []
+    r13s[DATA_ITEM_DATA] = data
+    for video_id in range(1, 5):
+        item = {"type": "movie", "id" : str(video_id)}
+        associated_items = [  {"type": "movie", "id" : str(other_video_id)} 
+                             for other_video_id in range (0, video_id) ]
+        data.append(format_recommendation(item, associated_items))
+    validate(r13s)
+    pprint.pprint(r13s)    
+
+if __name__ == '__main__':
+  main()
